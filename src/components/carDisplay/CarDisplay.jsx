b0fe44fb9320e7c20909aaf1b3306d2bbc524ea6
Wilson <wmurciarey9@gmail.com>
2023-04-17 14:15:08 -0500

    Added new car featured component

--- src/components/carDisplay/CarDisplay.jsx
+++ src/components/carDisplay/CarDisplay.jsx
@@ -1,17 +1,149 @@
 import React from "react";
+import Tesla2 from "../../images/tesla2.png";
+import CLA from "../../images/CLA-Coupe.png";
+import VW from "../../images/VW.png";
+
+import "./carDisplay.scss";
 
 export const CarDisplay = () => {
   return (
     <div className="carDisplay">
-      HELLO THIS IS THE CAR DISPLAY
-      <div className="div"></div>
+      <div className="carSection">
+        <img src={Tesla2} alt="" />
+        <div className="columns">
+          <div className="left">
+            <div className="leftComponent">
+              <h1>3.1 S </h1>
+              <p>0 - 60 mph</p>
+            </div>
+            <div className="leftComponent">
+              <h1>350 MI</h1>
+              <p>Range (EPA est.)</p>
+            </div>
+            <div className="leftComponent">
+              <h1>670 HP</h1>
+              <p>peak power</p>
+            </div>
+            <div className="leftComponent">
+              <h1 style={{ color: "green" }}>$159</h1>
+              <p>per day</p>
+            </div>
+          </div>
+
+          <div className="center">
+            <h1>Tesla Model 3</h1>
+            <button>See all cars</button>
+          </div>
+          <div className="right">
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              100+ miles autonomy
+            </div>
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              Multiple charging points
+            </div>
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              Full auto insurance coverage
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 export const CarDisplayTwo = () => {
   return (
     <div className="carDisplay">
-      HELLO THIS IS THE CAR DISPLAY 2<div className="div"></div>
+      <div className="carSection">
+        <img src={CLA} alt="" />
+        <div className="columns">
+          <div className="left">
+            <div className="leftComponent">
+              <h1>5.6 S </h1>
+              <p>0 - 60 mph</p>
+            </div>
+            <div className="leftComponent">
+              <h1>25 / 36 mpg</h1>
+              <p>Fuel economy</p>
+            </div>
+            <div className="leftComponent">
+              <h1>221 HP</h1>
+              <p>peak power</p>
+            </div>
+            <div className="leftComponent">
+              <h1 style={{ color: "green" }}>$107</h1>
+              <p>per day</p>
+            </div>
+          </div>
+
+          <div className="center">
+            <h1>2024 CLA Coupe</h1>
+            <button>See all cars</button>
+          </div>
+          <div className="right">
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              hybridized 2.0-liter four-cylinder
+            </div>
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>5 Seats
+            </div>
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              Full auto insurance coverage
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export const CarDisplayThree = () => {
+  return (
+    <div className="carDisplay">
+      <div className="carSection">
+        <img src={VW} alt="" />
+        <div className="columns">
+          <div className="left">
+            <div className="leftComponent">
+              <h1>7.8 s</h1>
+              <p>0 - 60 mph</p>
+            </div>
+            <div className="leftComponent">
+              <h1>24 / 36 mpg</h1>
+              <p>Fuel economy</p>
+            </div>
+            <div className="leftComponent">
+              <h1>170 HP</h1>
+              <p>Peak power</p>
+            </div>
+            <div className="leftComponent">
+              <h1 style={{ color: "green" }}>$25</h1>
+              <p>per day</p>
+            </div>
+          </div>
+
+          <div className="center">
+            <h1>VW Passat CC</h1>
+            <button>See all cars</button>
+          </div>
+          <div className="right">
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              4-cyl Turbo 2.0 liter
+            </div>
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>5 Seats
+            </div>
+            <div className="iconSet">
+              <i class="fa-solid fa-car"></i>
+              Full auto insurance coverage
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import "./featured.scss";
-import { CarDisplay, CarDisplayTwo } from "../carDisplay/CarDisplay";
+import {
+  CarDisplay,
+  CarDisplayThree,
+  CarDisplayTwo,
+} from "../carDisplay/CarDisplay";
 
 export const Featured = () => {
-  const [active, setActive] = useState("SecondCar");
+  const [active, setActive] = useState("ElectricCars");
+  const [colorTxt, setColorTxt] = useState("txt1");
+
+  const textID = (id) => {
+    setColorTxt(colorTxt === id ? "" : id);
+  };
+
+  const coloringTxt = (id) => {
+    return colorTxt === id ? "colored-text" : "";
+  };
 
   return (
     <div className="featured">
@@ -11,33 +24,40 @@ export const Featured = () => {
       <h1>Our Most Searched</h1>
       <div className="miniNavbar">
         <h2
-          className=""
+          className={`${coloringTxt("txt1")}`}
+          id="txt1"
           onClick={() => {
             setActive("ElectricCars");
+            textID("txt1");
           }}
         >
           Electric Favourite
         </h2>
         <h2
-          className=""
+          className={`${coloringTxt("txt2")}`}
+          id="txt2"
           onClick={() => {
             setActive("LuxuryCars");
+            textID("txt2");
           }}
         >
           Luxury Favourite
         </h2>
         <h2
-          className=""
+          className={`${coloringTxt("txt3")}`}
+          id="txt3"
           onClick={() => {
             setActive("CommonCars");
+            textID("txt3");
           }}
         >
           Common Favourite
         </h2>
       </div>
+
       {active === "ElectricCars" && <CarDisplay />}
       {active === "LuxuryCars" && <CarDisplayTwo />}
-      {active === "CommonCars" && <CarDisplay />}
+      {active === "CommonCars" && <CarDisplayThree />}
     </div>
   );
 };
